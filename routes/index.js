71fd79497c21cbe3bb84ca38c0e27ab4b2a4cee1
Hipolito Nahuel Souto <76236318+Nahuelhsouto@users.noreply.github.com>
2021-06-18 21:03:28 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-// importar express validator
+
 const { body } = require('express-validator/check');
 
-// importar el controlador
+
 const proyectosController = require('../controllers/proyectosController');
 const tareasController = require('../controllers/tareasController');
 const usuariosController = require('../controllers/usuariosController');
 const authController = require('../controllers/authController');
 
 module.exports = function() {
-    // ruta para el home
+  
     router.get('/', 
         authController.usuarioAutenticado,
         proyectosController.proyectosHome
@@ -27,13 +27,13 @@ module.exports = function() {
         proyectosController.nuevoProyecto
     );
 
-    // Listar Proyecto
+    
     router.get('/proyectos/:url', 
         authController.usuarioAutenticado,
         proyectosController.proyectoPorUrl
     );
 
-    // Actualizar el Proyecto
+    
     router.get('/proyecto/editar/:id', 
         authController.usuarioAutenticado,
         proyectosController.formularioEditar
@@ -45,43 +45,42 @@ module.exports = function() {
         proyectosController.actualizarProyecto
     );
 
-    // Eliminar Proyecto
+   
     router.delete('/proyectos/:url', 
         authController.usuarioAutenticado,
         proyectosController.eliminarProyecto
     );
 
-    // Tareas
+ 
     router.post('/proyectos/:url', 
         authController.usuarioAutenticado,
         tareasController.agregarTarea
     );
 
-    // Actualizar Tarea
+   
     router.patch('/tareas/:id', 
         authController.usuarioAutenticado,
         tareasController.cambiarEstadoTarea
     );
 
-    // Eliminar Tarea
+   
     router.delete('/tareas/:id', 
         authController.usuarioAutenticado,
         tareasController.eliminarTarea
     );
 
-    // Crear nueva cuenta
+   
     router.get('/crear-cuenta', usuariosController.formCrearCuenta);
     router.post('/crear-cuenta', usuariosController.crearCuenta);
     router.get('/confirmar/:correo', usuariosController.confirmarCuenta);
 
-    // iniciar sesión
     router.get('/iniciar-sesion', usuariosController.formIniciarSesion);
     router.post('/iniciar-sesion', authController.autenticarUsuario);
 
-    // cerrar sesion
+   
     router.get('/cerrar-sesion', authController.cerrarSesion);
 
-    // reestablecer contraseña
+    
     router.get('/reestablecer', usuariosController.formRestablecerPassword);
     router.post('/reestablecer', authController.enviarToken);
     router.get('/reestablecer/:token', authController.validarToken);
